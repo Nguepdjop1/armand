28fedaef2d3434855ccf6f84e53ae5b52d67e2e2
armand <nguepdjoparmand7@gmail.com>
2025-04-12 17:27:00 -0700

    o

--- resources/views/layouts/a.blade.php
+++ resources/views/layouts/a.blade.php
@@ -4,8 +4,10 @@
         <title>
             my laravel App
         </title>
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     </head>
     <boody>
+        <script src="{{mix('js/app.js') }}"></script>
         <header>
             <h1>
                 <marquee> mon application laravel</marquee>
